8a0b853545c9750f7e618d29494b1ba38a37688c
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2019-08-16 20:55:11 +0200

    Remove useless test and improve existing one.

--- test/unit/isTypeScriptTests.js
+++ test/unit/isTypeScriptTests.js
@@ -7,12 +7,8 @@ const assert = require('assertthat');
 const isTypeScript = require('../../lib/isTypeScript');
 
 suite('isTypeScript', () => {
-  test('throws error if no parameters are given.', async () => {
-    assert.that(() => isTypeScript()).is.throwing();
-  });
-
   test('throws error if directory is missing.', async () => {
-    assert.that(() => isTypeScript({})).is.throwing();
+    assert.that(() => isTypeScript({})).is.throwing('Directory is missing.');
   });
 
   test('is not built with typescript.', async () => {
